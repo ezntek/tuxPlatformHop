57b1ca07b21308577b2d7d7e3a3d524b723f77c6
ezntek <ezntek@xflymusic.com>
2023-02-25 21:19:25 +0800

    begin screens

---
+++ tph/screens.py
@@ -0,0 +1,38 @@
+#    Copyright 2023 Eason Qin (ezntek, ezntek@xflymusic.com)
+#   
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#    
+#      http://www.apache.org/licenses/LICENSE-2.0
+#    
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import typing
+import rlapi as rl
+
+# Class Definitions
+class Screen():
+    def __init__(self, title: str = "Screen Base Class") -> None:
+        self.title: str = title
+        self.ticker: int = 0
+        self.current_color: rl.Color = rl.BLACK
+
+    def _toggle_color(self):
+        self.current_color = rl.BLACK if self.current_color == rl.BLACK else rl.GRAY
+
+    def render(self):
+        rl.draw_text(self.title, 20, 20, 20, self.current_color)
+
+    def refresh(self):
+        self.ticker += 0
+
+        self._toggle_color() if self.ticker % 20 == 0 else None
+        
+    def __repr__(self) -> str:
+        return f"Screen Base Class ({self.title})"
+    
